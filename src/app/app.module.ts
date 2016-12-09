e82ef301f99dcecff02c5362602e48c9a0fb0822
martyns0n <zogacc@gmail.com>
2016-12-09 11:52:06 +0700

    Фикс отображения инпута файлов. Подгон отображения "Импорт" и "Письмо" страниц под ng2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { ImportComponent } from './import/import.component';
+import { LetterComponent } from './letter/letter.component';
+import { AnswersComponent } from './answers/answers.component';
+import { SenderComponent } from './sender/sender.component';
 
 const appRoutes: Routes = [
   { path: '', component: StartComponent },
-  { path: 'import', component: ImportComponent }
+  { path: 'import', component: ImportComponent },
+  { path: 'letter', component: LetterComponent },
+  { path: 'sender', component: SenderComponent },
+  { path: 'answers', component: AnswersComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    ImportComponent
+    ImportComponent,
+    LetterComponent,
+    AnswersComponent,
+    SenderComponent
   ],
   imports: [
     BrowserModule,
