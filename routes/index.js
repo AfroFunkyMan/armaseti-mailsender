88a031559edf1274a3af27d0381146e7529e1a82
AfroFunkyMan <afro.funky.lover@gmail.com>
2017-01-22 16:44:43 +0700

    Добавление пути и файл отписки

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/unsubscribe/:id', function(req, res, next) {
+    res.render('unsubscribe', { title: 'Express' });
+});
+
+
+
 module.exports = router;
