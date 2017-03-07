48b5275180a7ceeff300a34375e779e7034d88d9
AfroFunkyMan <afro.funky.lover@gmail.com>
2017-03-07 14:52:10 +0700

    Feature: Сохранение в базу вид активности - unsubscribe

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var router = express.Router();
-
+let MongoClient = require('mongodb').MongoClient;
+let ObjectID = require('mongodb').ObjectID;
+let db;
+MongoClient.connect('mongodb://localhost:27017/mailsender', function (err, db) {
+    console.log("Connected correctly to server");
+    db = db;
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/unsubscribe/:id', function(req, res, next) {
+    unsubThatID(req.params.id);
     res.render('unsubscribe', { title: 'Express' });
 });
 
-
+function unsubThatID(id) {
+    let _id = '';
+    try {
+        _id = ObjectID(id);
+    } catch (err) {
+        return null;
+    }
+    if (_id !== '') {
+        return db.collection('statistic').updateOne({"_id": _id},
+            {
+                $set: {
+                    status: 'unsubscribe'
+                },
+                $push: {
+                    action: 'unsubscribe',
+                    timestamp: new Date()
+                }
+            }, function (err, resp) {
+            return null;
+        })
+    }
+}
 
 module.exports = router;
