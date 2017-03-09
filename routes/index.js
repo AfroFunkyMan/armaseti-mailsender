2944142202c875a03436111abcebce37fadc6e00
AfroFunkyMan <afro.funky.lover@gmail.com>
2017-03-09 15:21:59 +0700

    Feature: Добавление трекера открытия

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 let MongoClient = require('mongodb').MongoClient;
 let ObjectID = require('mongodb').ObjectID;
-let contacts;
+let contacts, campaigns;
 MongoClient.connect('mongodb://localhost:27017/mailsender', function (err, db) {
     console.log("Connected correctly to server");
     contacts = db.collection('contacts');
+    campaigns = db.collection('campaign');
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,43 @@ router.get('/unsubscribe/:id', function(req, res, next) {
     unsubThatID(req.params.id);
     res.render('unsubscribe', { title: 'Express' });
 });
+
+
+
+router.get('/tracker/:contact/:campaign', function (req, res, next) {
+    var contact = req.params.contact,
+        campaign = req.params.campaign;
+    saveOpenAction(contact, campaign);
+    res.set('Content-Type', 'image/gif');
+    res.set('Connection', 'close');
+    res.send(new Buffer.from('R0lGODdhAQABAIAAAP///////ywAAAAAAQABAAACAkQBADs=','base64'));
+});
+
+
+function saveOpenAction(contactID, campaignID) {
+    let contactObjID = '';
+    let campaignObjID = '';
+    try {
+        let contactObjID = ObjectID(contactID);
+        let campaignObjID = ObjectID(campaignID);
+    }
+    catch (err) {
+        console.log(err);
+    }
+    if (contactObjID !== '' && campaignObjID !== ''){
+        campaigns.findOne({ _id: campaignObjID}, function (err, campaign) {
+            if (campaign) {
+                contacts.updateOne({_id: contactObjID}, {
+                    $push: {
+                        action: 'open',
+                        target: campaignObjID,
+                        timestamp: new Date()
+                    }
+                });
+            }
+        });
+    }
+}
 
 function unsubThatID(id) {
     let _id = '';
@@ -36,8 +74,7 @@ function unsubThatID(id) {
                 }
             }, function (err, resp) {
             return null;
-        })
+        });
     }
 }
-
 module.exports = router;
